aca64c177347660e34141d3b118561daa21df6ba
Xi Chen <xichen.u@gmail.com>
2020-01-29 22:56:49 -0500

    implement own redux bindActionCreators

---
+++ src/redux/bindActionCreators.js
@@ -0,0 +1,25 @@
+function bindActionCreator(actionCreator, dispatch) {
+  return (...args) => dispatch(actionCreator(...args));
+}
+
+// bindActionCreators is used in the implementation of react-redux connect;
+// It encapsulate dispatch and actionCreator
+export default function bindActionCreators(actionCreators, dispatch) {
+  if (typeof actionCreators === "function") {
+    return bindActionCreator(actionCreators, dispatch);
+  }
+
+  // actionCreators must be either function or object
+  if (typeof actionCreators !== "object" || actionCreators === null) {
+    throw new Error();
+  }
+
+  const boundActionCreations = {};
+  Object.keys(actionCreators).forEach(key => {
+    const actionCreator = actionCreators[key];
+    if (typeof actionCreator === "function") {
+      boundActionCreations[key] = bindActionCreator(actionCreator, dispatch);
+    }
+  });
+  return boundActionCreations;
+}
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,5 +2,12 @@ import createStore from "./createStore";
 import combineReducers from "./combineReducers";
 import applyMiddleware from "./applyMiddleware";
 import compose from "./compose";
+import bindActionCreators from "./bindActionCreators";
 
-export { createStore, combineReducers, applyMiddleware, compose };
+export {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+  bindActionCreators
+};
